bc1ccafb5f1ea9a13a34e0528a34f5b91524e066
WhiteMasterYuraCPP-20 <yuravolchkov@gmail.com>
2024-05-25 21:04:59 +0300

    correction4 25 05 24 Commit

--- hw31/hw31.cpp
+++ hw31/hw31.cpp
@@ -1,13 +1,9 @@
-﻿// hw31.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
-
+﻿#include <iostream>
 
 class Calculator
 {
 private:
-    double num1=1, num2=1;
+    double num1, num2;
 
 public:
     Calculator(double num1_from_main, double num2_from_main) : num1(num1_from_main), num2(num2_from_main) {}
@@ -36,6 +32,33 @@ public:
     {
         return num2 / num1;
     }
+
+    bool set_num1(double num1)
+    {
+        if (num1 == 0)
+        {
+            std::cout << "Неверный ввод первого числа!\n";
+            return 0;
+        }
+        else
+        {
+            this->num1 = num1;
+            return 1;
+        }
+    }
+    bool set_num2(double num2)
+    {
+        if (num2 == 0)
+        {
+            std::cout << "Неверный ввод второго числа!\n";
+            return 0;
+        }
+        else
+        {
+            this->num2 = num2;
+            return 1;
+        }
+    }
 };
 
 int main()
@@ -46,50 +69,20 @@ int main()
     double num1_from_main, num2_from_main;
     for (;;) //бесконечный цикл
     {
-        for (bool i = 0; i != 1;) //ввод первого числа с проверкой "ненулевого значения"
+        std::cout << "Введите num1: ";
+        std::cin >> num1_from_main;
+        std::cout << "Введите num2: ";
+        std::cin >> num2_from_main;
+        Calculator counting{ num1_from_main, num2_from_main };
+        if (counting.set_num1(num1_from_main) == 1 && counting.set_num2(num2_from_main) == 1)
         {
-            std::cout << "Введите num1: ";
-            std::cin >> num1_from_main;
-            if (num1_from_main == 0)
-            {
-             std::cout << "Неверный ввод! \n";
-            }
-            else {
-                i = 1;
-            }
-
+            std::cout << "num1 + num2 = " << counting.add() << std::endl;
+            std::cout << "num1 * num2 = " << counting.multiply() << std::endl;
+            std::cout << "num1 - num2 = " << counting.subtract_1_2() << std::endl;
+            std::cout << "num2 - num1 = " << counting.subtract_2_1() << std::endl;
+            std::cout << "num1 / num2 = " << counting.divide_1_2() << std::endl;
+            std::cout << "num2 / num1 = " << counting.divide_2_1() << std::endl;
         }
-        for (bool i = 0; i != 1;) //ввод второго числа с проверкой "ненулевого значения"
-        {
-            std::cout << "Введите num2: ";
-            std::cin >> num2_from_main;
-            if (num2_from_main == 0)
-            {
-                std::cout << "Неверный ввод! \n";
-            }
-            else {
-                i = 1;
-            }
-        }
-        Calculator counting{num1_from_main, num2_from_main};
-        
-        std::cout << "num1 + num2 = " << counting.add() << std::endl;
-        std::cout << "num1 * num2 = " << counting.multiply() << std::endl;
-        std::cout << "num1 - num2 = " << counting.subtract_1_2() << std::endl;
-        std::cout << "num2 - num1 = " << counting.subtract_2_1() << std::endl;
-        std::cout << "num1 / num2 = " << counting.divide_1_2() << std::endl;
-        std::cout << "num2 / num1 = " << counting.divide_2_1() << std::endl;
     }
     return 31;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
